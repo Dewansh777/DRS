19bbee3a3c80d2e81cbef3f5a6d5a5524aed69c3
Dewansh777 <dewanshmishra777@gmail.com>
2025-01-09 02:56:44 +0530

    Update Procfile to use dynamic PORT and modify app.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+gunicorn -b 0.0.0.0:$PORT src.app:app
--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model import search_embeddings
 import logging
-
+import os
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -61,4 +61,5 @@ def get_recommendation():
 
 if __name__ == "__main__":
     logger.info("Starting Flask application...")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
